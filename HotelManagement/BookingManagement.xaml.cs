96986db2d0ac25d309eeaba49c07dd8be60188a4
Giangdz102 <gianghy2005@gmail.com>
2026-07-08 21:52:58 +0700

    new BookingManagement

---
+++ HotelManagement/BookingManagement.xaml.cs
@@ -0,0 +1,54 @@
+﻿using HotelManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement
+{
+    /// <summary>
+    /// Interaction logic for BookingManagement.xaml
+    /// </summary>
+    public partial class BookingManagement : Window
+    {
+        public BookingManagement()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (var db = new FuminiHotelManagementContext())
+            {
+                LvBookings.ItemsSource = db.BookingReservations.ToList();
+            }
+        }
+
+        private void LvBookings_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LvBookings.SelectedItem is BookingReservation selectedBooking)
+            {
+                using (var db = new FuminiHotelManagementContext())
+                {
+                    LvBookingDetails.ItemsSource = db.BookingDetails
+                        .Where(bd => bd.BookingReservationId == selectedBooking.BookingReservationId)
+                        .ToList();
+                }
+            }
+            else
+            {
+                LvBookingDetails.ItemsSource = null;
+            }
+        }
+    }
+}
--- HotelManagement/obj/Debug/net8.0-windows/HotelManagement.AssemblyInfo.cs
+++ HotelManagement/obj/Debug/net8.0-windows/HotelManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f3f45994f042ef44b899f45c2dae4470d23400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99aa0ac63136688390510edf346d189913767276")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
